106314c329bf4287f39704fb3255e7940a7a4cab
Joy <github.joysaha@gmail.com>
2024-02-02 12:05:48 +0600

    second practice js

--- practice.js
+++ practice.js
@@ -13,4 +13,19 @@
 
 // console.log(colors);
 
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
 
+// let evenNumbers = [];
+
+// for(i = 0; i < numbers.length; i++){
+//     if(numbers[i] % 2 === 0){
+//         evenNumbers.push(numbers[i]);
+//     }
+// }
+// console.log(evenNumbers);
+
+var numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+
+let text = numbers.join('');
+
+console.log(text)
--- sortArray.js
+++ sortArray.js
@@ -1,12 +1,21 @@
 const persons = ['rakib', 'nokib', 'sakib', 'akib', 'dakib'];
 // const sortedPersons = persons.sort();
-// console.log(sortedPersons);
+// // console.log(sortedPersons);
 
-const numbers = [3, 5, 23, 9, 45, 9,10, 4,9,54,];
+// const numbers = [3, 5, 23, 9, 45, 9,10, 4,9,54,];
 
-const numbers_asc = [...numbers].sort(function (a, b) { return a - b})
-const numbers_dsc = [...numbers].sort(function (a, b) { return b - a})
+// const numbers_asc = [...numbers].sort(function (a, b) { return a - b})
+// const numbers_dsc = [...numbers].sort(function (a, b) { return b - a})
 
-console.log(numbers_asc);
-console.log(numbers_dsc);
-console.log(numbers_dsc);
+// console.log(numbers_asc);
+// console.log(numbers_dsc);
+// console.log(numbers_dsc);
+
+var names = ['Tom', 'Tim', 'Tin', 'Tik'];
+var concatenatedString = '';
+
+for (const name of names) {
+  concatenatedString += name + ' ';
+}
+
+console.log(concatenatedString.trim());
