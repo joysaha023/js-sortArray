0a61b63a1503106ba95c4f159b613d94ced00af9
Joy <github.joysaha@gmail.com>
2024-02-02 11:54:49 +0600

    practise 1 commit code

---
+++ practice.js
@@ -0,0 +1,16 @@
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+
+// function reverseColors(array) {
+//     const length = array.length;
+//     for(let i = 0; i < Math.floor(length / 2); i++){
+//         const temp = array[i];
+//         array[i] = array[length - 1 - i];
+//         array[length - 1 - i] = temp;
+//     }
+// }
+
+// reverseColors(colors);
+
+// console.log(colors);
+
+
